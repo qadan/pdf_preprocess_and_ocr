be6b9c19ffbcde1c228a5edc8cb512a4c0e1443b
daitken <daitken@discoverygarden.ca>
2013-03-18 23:20:39 -0300

    more verbose, better cleanup

--- ocrpreprocess.bat
+++ ocrpreprocess.bat
@@ -1,8 +1,16 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
-mogrify -format tiff -colorspace gray -density 300 -fill white -background white +matte *.pdf
+echo PDF_PREPROCESS_AND_OCR
+for /f "tokens=*" %%t in ('dir /b *.pdf') do (
+  set filename=%%~nt
+  echo Preprocessing "!filename!" ...
+  mogrify -format tiff -colorspace gray -density 300 -fill white -background white +matte "%%t"
+  )
 for /f "tokens=*" %%p in ('dir /b *.tif') do (
   set filename=%%~np
-	tesseract "%%p" "!filename!"
-	)
+  echo Performing OCR on "!filename!" ...
+  tesseract "%%p" "!filename!"
+  echo Cleaning up ...
+  del "%%p"
+  )
 ENDLOCAL
